1a2952bc3781d8dc6055536e5d63ebc3d7144da1
Jeremías Martínez <jeremiasmartinezdiez1@gmail.com>
2022-11-18 17:35:06 -0300

    mod

--- ECUACION_DLA.py
+++ ECUACION_DLA.py
@@ -3,21 +3,22 @@ import matplotlib.pyplot as plt
 import random
 
 #resolución de la imagen
-altura_columnas=30
-ancho_filas=30
+altura_columnas=50
+ancho_filas=50
 
-R=random.uniform(0.0,1)
+
 P=np.zeros([ancho_filas,altura_columnas])
-P[15,15]=1.0
+P[25,25]=1.0
+for t in range(500):
+    plt.imshow(P.T,cmap='Greys')
+    R=random.uniform(0,0.01)
 
-for i in range(29):
-    for j in range(29):
-        P[i,j]=0.25*(P[i+1,j]+P[i-1,j]+P[i,j+1]+P[i,j-1])
-        
-plt.figure(figsize=(5,5))
-plt.imshow(P.T,cmap='Greys')
-plt.ylim(0,29)
-plt.xlim(0,29)
+    for i in range(49):
+        for j in range(49):
+            P[i,j]=0.25*(P[i+1,j]+P[i-1,j]+P[i,j+1]+P[i,j-1]+R)
+
+plt.ylim(0,49)
+plt.xlim(0,49)
 plt.xlabel("x")
 plt.ylabel("y")
 plt.show()
