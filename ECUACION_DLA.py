b6ed9cd189c9059748a8eaaa27490eeecd4e2b22
Jeremías Martínez <jeremiasmartinezdiez1@gmail.com>
2022-11-23 22:08:24 -0300

    actualizacion codigo DLA

--- ECUACION_DLA.py
+++ ECUACION_DLA.py
@@ -1,24 +1,37 @@
+from random import choice
 import numpy as np
 import matplotlib.pyplot as plt
-import random
 
-#resolución de la imagen
-altura_columnas=50
-ancho_filas=50
+npart=10000 #Numero de particulas del agregado
+tama=150 #tamaño de la imagen
+dir = [(1,0),(-1,0),(0,1),(0,-1)] #direcciones para la difusion
 
 
-P=np.zeros([ancho_filas,altura_columnas])
-P[25,25]=1.0
-for t in range(500):
-    plt.imshow(P.T,cmap='Greys')
-    R=random.uniform(0,0.01)
 
-    for i in range(49):
-        for j in range(49):
-            P[i,j]=0.25*(P[i+1,j]+P[i-1,j]+P[i,j+1]+P[i,j-1]+R)
+grid=[[0 for x in range(tama)] for y in range(tama)]
+grid[int(tama/2)][int(tama/2)]=1
+for ipart in range(npart):
+    # 
+    x,y = 0,0
+    # Inicio de la caminata aleatoria
+    while 1:
+        grid[x][y]=0 #Eliminar partículas del lugar actual
+        # Movimiento de particulas aleatorias
+        sx,sy = choice(dir)
+        x += sx
+        y += sy
+        # Caminos donde se añaden las particulas
+        if x < 0: 
+            x=tama-1
+        if y < 0: 
+            y=tama-1
+        if x==tama: 
+            x=0
+        if y==tama: 
+            y=0
+        grid[x][y]=1 #Pone la partícula en una nueva ubicación
+        # se detiene si está al lado de una partícula
+        if (grid[(x+1)%tama][y]+grid[x][(y+1)%tama]+grid[(x+tama-1)%tama][y]+grid[x][(y+tama-1)%tama])>0:
+            break
 
-plt.ylim(0,49)
-plt.xlim(0,49)
-plt.xlabel("x")
-plt.ylabel("y")
-plt.show()
+plt.imshow(grid,interpolation="bilinear",cmap="gray")
